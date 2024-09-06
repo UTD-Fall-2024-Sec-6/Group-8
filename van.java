c7adbf18107fa0f2e8fdc543eb7860ae4c574437
Van Le <vtl230001@utdallas.edu>
2024-09-05 23:01:00 -0500

    Van Le

---
+++ van.java
@@ -0,0 +1,8 @@
+public class GoalGrid
+{
+    public static void main(String[] args) {
+            'Class' 'objName' = new 'Class'();
+        System.out.println("Hello, I'm Van");
+        System.out.println('objName'.x);
+    }
+}
