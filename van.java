1eea52078c4f1ac8204787f6ac1e276cb4044d76
AmmarMohammed01 <118853010+AmmarMohammed01@users.noreply.github.com>
2024-09-06 19:11:03 -0500

    Update van.java

--- van.java
+++ van.java
@@ -1,8 +1,18 @@
 public class GoalGrid
 {
     public static void main(String[] args) {
-            'Class' 'objName' = new 'Class'();
+        Erin erin = new Erin();
+        Ammar ammar = new Ammar();
+        Quynh quynh = new Quynh();
+        Sneha sneha = new Sneha();
+        Benjamin benjamin = new Benjamin();
+        
         System.out.println("Hello, I'm Van");
         System.out.println('objName'.x);
+        System.out.println(erin.x);
+        System.out.println(ammar.x);
+        System.out.println(quynh.x);
+        System.out.println(sneha.x);
+        System.out.println(benjamin.x);
     }
 }
