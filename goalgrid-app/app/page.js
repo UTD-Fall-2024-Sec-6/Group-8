c5e8fb67959ebef5103cd1ce57752ac3f21aed8b
Ammar Mohammed <ammar.hm321@gmail.com>
2024-11-04 23:36:55 -0600

    Worked on UI

--- goalgrid-app/app/page.js
+++ goalgrid-app/app/page.js
@@ -1,10 +1,25 @@
 import styles from "./styles.css";
+import { IoMdSettings } from "react-icons/io";
+//https://react-icons.github.io/react-icons/search/#q=settings
+//npm install react-icons
+//https://stackoverflow.com/questions/43768629/how-to-scale-large-font-awesome-icons-from-the-react-icons-package
 
 export default function Home() {
   return (
     <>
-      <h1>Goal Creation Page</h1>
-      <input placeholder="Type goal name here"></input>
+      <div className="GoalGridAppLayout">
+
+        <div className="SideMenu">
+          <div> <h1>GoalGrid</h1> </div>
+          <div> <button className="MenuButton">Edit/Add Goals</button> </div>
+          <div> <button className="MenuButton">Generate New Grid</button> </div>
+          <div> <button className="SettingsButton"><IoMdSettings size={42}/></button> </div>
+        </div>
+
+        <div className="Grid"></div>
+
+      </div>
+      
     </>
   );
 }
