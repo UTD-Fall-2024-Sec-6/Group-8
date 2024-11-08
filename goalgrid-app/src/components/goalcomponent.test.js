05a6963681ce75166fe1d74b32ab2d3684d58ece
Ammar Mohammed <ammar.hm321@gmail.com>
2024-11-08 07:38:57 -0600

    Commented test case file

--- goalgrid-app/src/components/goalcomponent.test.js
+++ goalgrid-app/src/components/goalcomponent.test.js
@@ -4,16 +4,19 @@ import GoalComponent from "./goalcomponent"
 import { Goal } from '../pages/goal.js';
 
 describe(GoalComponent, () => {
+  //Test 1 for Add Goal (UC4): 
   it("User passes empty input, goal description should say 'Empty Goal'", () => {
     const { getByTestID } = render(<GoalComponent aGoal={new Goal(Math.random(), "", false)} completeGoal={null}/>)
     const goalDescription = screen.getByTestId("description").textContent;
     expect(goalDescription).toEqual("Empty Goal");
   })
-  
+
+  //Test 1 for Mark Complete (UC9)
   it("Test #1 for Mark Complete: When goal is initially false, button click, output should be true", () => {
     //Code goes here
   })
 
+  //Test 2 for Mark Complete (UC9)
   it("Test #2 for Mark Complete: When goal is initially true, button click, output should be false", () => {
     //Code goes here
   })
