349a0618623b2312f476429eca9eb54ac445ee2d
Ammar Mohammed <ammar.hm321@gmail.com>
2024-11-08 09:47:26 -0600

    Code for UC9 test case

--- goalgrid-app/src/pages/goal.test.js
+++ goalgrid-app/src/pages/goal.test.js
@@ -4,15 +4,15 @@ import { Goal } from '../pages/goal.js';
 describe(Goal, () => {
   //Test 1 for Mark Complete (UC9)
   it("Test #1 for Mark Complete: When goal is initially false, button click, output should be true", () => {
-    
-    //code here
-
+    const testGoal = new Goal(Math.random(), "Test 1", false);
+    testGoal.markComplete();
+    expect(testGoal.isCompleted).toBe(true);
   })
 
   //Test 2 for Mark Complete (UC9)
   it("Test #2 for Mark Complete: When goal is initially true, button click, output should be false", () => {
-    
-    //code here
-
+    const testGoal = new Goal(Math.random(), "Test 1", true);
+    testGoal.markComplete();
+    expect(testGoal.isCompleted).toBe(false);
   })
 })
